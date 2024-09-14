df5ab7995b80f4e7a8a722a2ab0cebe2ece77ec8
diogosfpinto <diogo.sfpinto@gmail.com>
2024-09-14 15:37:23 -0400

    add embedded database initialization

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,1 +1,5 @@
-INSERT INTO cliente (id, limite, saldoInicial) VALUES ('1', '100000', '0');
+INSERT INTO clientes (id, limite, saldo_Inicial) VALUES ('1', '100000', '0');
+INSERT INTO clientes (id, limite, saldo_Inicial) VALUES ('2', '80000', '0');
+INSERT INTO clientes (id, limite, saldo_Inicial) VALUES ('3', '1000000', '0');
+INSERT INTO clientes (id, limite, saldo_Inicial) VALUES ('4', '10000000', '0');
+INSERT INTO clientes (id, limite, saldo_Inicial) VALUES ('5', '500000', '0');
